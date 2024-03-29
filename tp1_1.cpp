5f3baa9e73494b0a20a7eeb92d3abc38e72ed5da
Miguel <chetobustos96@gmail.com>
2024-03-29 14:49:26 -0300

    Ejercicio 3)e) parte 1

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -3,5 +3,11 @@
 int main(int argc, char const *argv[])
 {
     printf("Hola Mundo");
+    int x = 10;
+
+    int *puntero = &x;
+
+    std::cout << "1) Contenido del puntero: " << *puntero << std::endl;
+    
     return 0;
 }
