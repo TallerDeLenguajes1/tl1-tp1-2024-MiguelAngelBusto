bc7a8cf888720c9931b028129cbfdfcc6279a39e
Miguel <chetobustos96@gmail.com>
2024-03-29 14:50:15 -0300

    Ejercicio 3)e) parte 2

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -8,6 +8,9 @@ int main(int argc, char const *argv[])
     int *puntero = &x;
 
     std::cout << "1) Contenido del puntero: " << *puntero << std::endl;
+
+    std::cout << "2) Dirección de memoria almacenada por el puntero: " << puntero << std::endl;
+
     
     return 0;
 }
